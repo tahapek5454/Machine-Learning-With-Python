043bc1b8d91e9049239ee1eb835791a9a49117b5
Taha Pek <tahapek5454@gmail.com>
2022-08-21 17:29:58 +0300

    add odev

---
+++ siniflandirma(classification)/odev/_odev.py
@@ -0,0 +1,177 @@
+#1.kutuphaneler
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+# veriyi gorme
+# unused but required import for doing 3d projections with matplotlib < 3.2
+import mpl_toolkits.mplot3d  # noqa: F401
+
+from sklearn import datasets
+from sklearn.decomposition import PCA
+
+# import some data to play with
+iris = datasets.load_iris()
+X = iris.data[:, :2]  # we only take the first two features.
+y = iris.target
+
+x_min, x_max = X[:, 0].min() - 0.5, X[:, 0].max() + 0.5
+y_min, y_max = X[:, 1].min() - 0.5, X[:, 1].max() + 0.5
+
+plt.figure(2, figsize=(8, 6))
+plt.clf()
+
+# Plot the training points
+plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.Set1, edgecolor="k")
+plt.xlabel("Sepal length")
+plt.ylabel("Sepal width")
+
+plt.xlim(x_min, x_max)
+plt.ylim(y_min, y_max)
+plt.xticks(())
+plt.yticks(())
+
+# To getter a better understanding of interaction of the dimensions
+# plot the first three PCA dimensions
+fig = plt.figure(1, figsize=(8, 6))
+ax = fig.add_subplot(111, projection="3d", elev=-150, azim=110)
+
+X_reduced = PCA(n_components=3).fit_transform(iris.data)
+ax.scatter(
+    X_reduced[:, 0],
+    X_reduced[:, 1],
+    X_reduced[:, 2],
+    c=y,
+    cmap=plt.cm.Set1,
+    edgecolor="k",
+    s=40,
+)
+
+ax.set_title("First three PCA directions")
+ax.set_xlabel("1st eigenvector")
+ax.w_xaxis.set_ticklabels([])
+ax.set_ylabel("2nd eigenvector")
+ax.w_yaxis.set_ticklabels([])
+ax.set_zlabel("3rd eigenvector")
+ax.w_zaxis.set_ticklabels([])
+
+plt.show()
+
+#2.veri onisleme
+#2.1.veri yukleme
+veriler = pd.read_excel(r"C:\Users\90543\Desktop\VS\machine-leraning-python\datas\Iris.xls")
+#pd.read_csv("veriler.csv")
+#test
+print(veriler)
+
+x = veriler.iloc[:,:4].values #bağımsız değişkenler
+y = veriler.iloc[:,4:].values #bağımlı değişken
+
+
+#verilerin egitim ve test icin bolunmesi
+from sklearn.model_selection import train_test_split
+
+x_train, x_test,y_train,y_test = train_test_split(x,y,test_size=0.33, random_state=0)
+
+#verilerin olceklenmesi
+from sklearn.preprocessing import StandardScaler
+
+sc=StandardScaler()
+
+X_train = sc.fit_transform(x_train)
+X_test = sc.transform(x_test)
+
+
+from sklearn.linear_model import LogisticRegression
+logr = LogisticRegression(random_state=0)
+logr.fit(X_train,y_train)
+
+y_pred = logr.predict(X_test)
+
+
+
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test,y_pred)
+print('LR')
+print(cm)
+
+
+
+from sklearn.neighbors import KNeighborsClassifier
+
+knn = KNeighborsClassifier(n_neighbors=1, metric='minkowski')
+knn.fit(X_train,y_train)
+
+y_pred = knn.predict(X_test)
+
+cm = confusion_matrix(y_test,y_pred)
+print('KNN')
+print(cm)
+
+
+
+from sklearn.svm import SVC
+svc = SVC(kernel='rbf')
+svc.fit(X_train,y_train)
+
+y_pred = svc.predict(X_test)
+
+cm = confusion_matrix(y_test,y_pred)
+print('SVC')
+print(cm)
+
+
+
+from sklearn.naive_bayes import GaussianNB
+gnb = GaussianNB()
+gnb.fit(X_train, y_train)
+
+y_pred = gnb.predict(X_test)
+
+cm = confusion_matrix(y_test,y_pred)
+print('GNB')
+print(cm)
+
+
+from sklearn.tree import DecisionTreeClassifier
+dtc = DecisionTreeClassifier(criterion = 'entropy')
+
+dtc.fit(X_train,y_train)
+y_pred = dtc.predict(X_test)
+
+cm = confusion_matrix(y_test,y_pred)
+print('DTC')
+print(cm)
+
+
+from sklearn.ensemble import RandomForestClassifier
+rfc = RandomForestClassifier(n_estimators=10, criterion = 'entropy')
+rfc.fit(X_train,y_train)
+
+y_pred = rfc.predict(X_test)
+cm = confusion_matrix(y_test,y_pred)
+print('RFC')
+print(cm)
+
+
+    
+# 7. ROC , TPR, FPR değerleri 
+# print('Roc Tpr Fpr')
+
+# y_proba = rfc.predict_proba(X_test)
+# print(y_test)
+# print(y_proba[:,0])
+
+# from sklearn import metrics
+# fpr , tpr , thold = metrics.roc_curve(y_test,y_proba[:,0],pos_label='e')
+# print(fpr)
+# print(tpr)
+
+
+
+
+
+
+
+
