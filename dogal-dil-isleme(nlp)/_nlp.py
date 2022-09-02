d7cd34d6b3d712dc19a629f6b17180376697a1c3
Taha Pek <tahapek5454@gmail.com>
2022-09-02 13:53:02 +0300

    add nlp

--- dogal-dil-isleme(nlp)/_nlp.py
+++ dogal-dil-isleme(nlp)/_nlp.py
@@ -1,12 +1,51 @@
 import numpy as np
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
 yorumlar = pd.read_csv(r'C:\Users\90543\Desktop\VS\machine-leraning-python\datas\yorumlar.csv')
 
 print(yorumlar)
 
 import re
+import nltk
 
-#noktalama isaretlerini kaldiracagiz
-yorum = re.sub('[^a-zA-Z]',' ',yorumlar['Review'][0])
-print(yorum)
+from nltk.stem.porter import PorterStemmer
+ps = PorterStemmer()
+
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+
+#Preprocessing (Önişleme)
+derlem = []
+for i in range(49):
+    yorum = re.sub('[^a-zA-Z]',' ',yorumlar['Review'][i])
+    yorum = yorum.lower()
+    yorum = yorum.split()
+    yorum = [ps.stem(kelime) for kelime in yorum if not kelime in set(stopwords.words('english'))]
+    yorum = ' '.join(yorum)
+    derlem.append(yorum)
+
+
+#Feautre Extraction ( Öznitelik Çıkarımı)
+#Bag of Words (BOW)
+from sklearn.feature_extraction.text import CountVectorizer
+cv = CountVectorizer(max_features = 1000)
+X = cv.fit_transform(derlem).toarray() # bağımsız değişken
+y = yorumlar.iloc[:,1].values # bağımlı değişken
+
+
+x_train, x_test,y_train,y_test = train_test_split(X,y,test_size=0.20, random_state=0)
+
+print(x_train)
+
+
+#machineLearning
+from sklearn.naive_bayes import GaussianNB
+gnb = GaussianNB()
+gnb.fit(x_train,y_train)
+
+y_pred = gnb.predict(x_test)
+
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test,y_pred)
+print(cm)
